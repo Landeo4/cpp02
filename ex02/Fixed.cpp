dc7b9d4da1ba684f11e990a9cef6c62fd74bbd0b
Theo POTILLION <tpotilli@made-f0Br4s10.clusters.42paris.fr>
2024-06-05 10:59:02 +0200

    viral

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,86 @@
+#include "Fixed.hpp"
+
+const int Fixed:: _nb2 = 8;
+
+Fixed:: Fixed(void) : _nb(0)
+{
+	std::cout << "default constructor called" << std::endl;
+	return ;
+}
+
+Fixed:: ~Fixed(void)
+{
+	std::cout << "destructor called" << std::endl;
+	return ;
+}
+
+Fixed:: Fixed(const Fixed &nb)
+{
+	std::cout << "copy constructor called" << std::endl;
+	*this = nb;
+}
+
+Fixed &Fixed::operator =(const Fixed &b)
+{
+	std::cout << "copy assignement called" << std::endl;
+	if (this != &b)
+	{
+		this->_nb = b.getRawBits();
+	}
+	return *this;
+}
+
+Fixed::Fixed(const int &numb)
+{
+	std::cout << "int constructor called" << std::endl;
+	this->_nb = (int)(numb << this->_nb2);
+}
+
+// doit convert a la numb en nb a virgule
+// regarder les video pour s'aider
+
+Fixed::Fixed(const float &numb)
+{
+	std::cout << "float constructor called" << std::endl;
+	this->_nb = float(numb * (1 << this->_nb2));
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)this->_nb / (float)(1 << this->_nb2));
+}
+
+int Fixed::toInt(void) const
+{
+	return ((int)this->_nb / (int)(1 << this->_nb2));
+}
+// doit tranformer le nombre a virgule en entier
+
+// je dois aussi ajouter une fonction qui surcharge << qui envois un float de la valeur
+// vers l'ouput (print quoi) passer en parametre
+
+int Fixed::getRawBits(void) const
+{
+	return this->_nb;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_nb = raw;
+}
+
+std::ostream & operator<<( std::ostream & o, Fixed const & nb)
+{
+	o << nb.toFloat();
+	return o;
+}
+
+//pour BSP utiliser la methode Vectoriel
+// il y a deux Vecteurs qui te dise avec des rotations un point
+
+// potentiellement garder int32_t
+// e = (int64_t(a) * int64_t(b)) >> 16;
+//24 pour entier
+//8 pour fractionnaire
+// la moitier des bits du int 4 sup a virgule 4 autre pour le reste
+// 4.78 -> 4 = 4 bits et 78 4 dernier bits -> 8bits int
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,36 @@
+#ifndef FIXED_HH
+#define FIXED_HH
+
+#include <string.h>
+#include <iostream>
+
+class Fixed
+{
+	public:
+	Fixed(void);
+	Fixed(const Fixed &nb);
+	Fixed &operator=(const Fixed &b);
+	Fixed(const int &numb);
+	Fixed(const float &numb);
+	std::ostream & operator>(std::ostream & o, Fixed const & nb);
+	std::ostream & operator<(std::ostream & o, Fixed const & nb);
+	std::ostream & operator>=(std::ostream & o, Fixed const & nb);
+	std::ostream & operator<=(std::ostream & o, Fixed const & nb);
+	std::ostream & operator==(std::ostream & o, Fixed const & nb);
+	std::ostream & operator!=(std::ostream & o, Fixed const & nb);
+	std::ostream & operator+(std::ostream & o, Fixed const & nb);
+	std::ostream & operator-(std::ostream & o, Fixed const & nb);
+	std::ostream & operator*(std::ostream & o, Fixed const & nb);
+	std::ostream & operator/(std::ostream & o, Fixed const & nb);
+	~Fixed(void);
+	float toFloat(void) const;
+	int toInt(void) const;
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	private:
+	int _nb;
+	static const int _nb2;
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & nb);
+#endif
